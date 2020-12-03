399f035100c0dd965f48c8992cdcd23d8b6e9738
sara <sara.sennouni1996@gmail.com>
2020-12-03 12:30:46 +0100

    test

---
+++ pages/api/test.js
@@ -0,0 +1,9 @@
+import dbConnect from "../../utils/dbConnect";
+
+dbConnect();
+
+export default async (req, res) => {
+    res.json({
+        test: 'test'
+    });
+}
---
+++ utils/dbConnect.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+async  function  dbConnect() {
+    if(connection.isConnected){
+        return;
+    }
+    const db = await mongoose.connect('mongodb://localhost:27017/test');
+    connection.isConnected = db.connection[0].readyState;
+    console.log(connection.isConnected);
+}
+export default dbConnect;
